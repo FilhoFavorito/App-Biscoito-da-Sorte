f1ee3e5690ebefbde997bb935694c7a80ae073e3
Joao Guesser <joaopedroguesser902@gmail.com>
2026-04-27 21:34:01 -0300

    App Biscoito funcionando

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, View, StyleSheet, TouchableOpacity} from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Image} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const vetorFrases = [
@@ -13,45 +13,84 @@ const vetorFrases = [
 ];
 
 export default function Index() {
+  const [opened, setOpened] = useState(false);
   const [frase, setFrase] = useState("");
+
   function aoPressionar() {
-    return setFrase(vetorFrases[Math.floor(Math.random() * (6 - 0) + 0)]);
+     setFrase(vetorFrases[Math.floor(Math.random() * (6 - 0) + 0)]);
+     setOpened(true);
+
   }
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
+<SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
 
-        <View style={styles.fraseContainer}>
-          <Text>Frase da Sorte: {frase}</Text>
-        </View>
+      <View style={styles.fraseContainer}>
         
-        <TouchableOpacity style={styles.botao} onPress={aoPressionar}>
-          <Text>Gerar Frase</Text>
-        </TouchableOpacity>
+        {opened ? (
+          <View style={styles.biscoitoAbertoContainer}>
+            <Image style={styles.biscoitoAberto} source={require("../assets/images/biscoito-aberto1.png")}/>
+            <Text style={styles.fraseTexto}>{frase}</Text>
+            <Image style={styles.biscoitoAberto} source={require("../assets/images/biscoito-aberto2.png")}/>
+          </View>
+        ) : (
+          <Image style={styles.biscoitoFechado} source={require("../assets/images/biscoito-fechado.png")}/>
+        )}
 
-      </SafeAreaView>  
-    </SafeAreaProvider>
+      </View>
+      
+      <TouchableOpacity style={styles.botao} onPress={aoPressionar}>
+        <Text>Gerar Frase</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>  
+  </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: "center"
   },
-  fraseContainer:{
+
+  fraseContainer: {
     alignItems: 'center',
     padding: 10,
   },
-  botao:{
+
+  biscoitoAbertoContainer: {
+    flex: 3,
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignItems: "center"
+  },
+
+  biscoitoAberto: {
+    width: 75,
+    height: 150
+  },
+
+  fraseTexto: {
+    width: 300,
+    textAlign: 'center'
+  },
+
+  biscoitoFechado: {
+    width: 150,
+    height: 150
+  },
+
+  botao: {
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
     padding: 10,
   },
+
   biscoito: {
-    width:250,
-    height:250
+    width: 250,
+    height: 250
   }
 });
